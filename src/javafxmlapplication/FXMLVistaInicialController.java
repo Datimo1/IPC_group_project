0fa1e4e78abe3f7914308d9022d86595484d47c6
Datimo1 <david.atimo1@gmail.com>
2023-12-31 01:44:11 +0100

    Cambios sobre todo en el css de la interfaz principal

--- src/javafxmlapplication/FXMLVistaInicialController.java
+++ src/javafxmlapplication/FXMLVistaInicialController.java
@@ -27,16 +27,6 @@ public class FXMLVistaInicialController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        readyButton.setOnAction((event) -> {
-         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-// ó AlertType.WARNING ó AlertType.ERROR ó AlertType.CONFIRMATION
-        alert.setTitle("Esa es");
-        alert.setHeaderText("Yes why not");
-// ó null si no queremos cabecera
-        alert.setContentText("Siu");
-        alert.showAndWait();
-        
-        });
     }    
     
 }
