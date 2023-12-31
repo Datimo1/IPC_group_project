d3d5ff432ce67e8fd56081f313dc0ba6057ff016
Datimo1 <david.atimo1@gmail.com>
2023-12-31 13:22:49 +0100

    transicion entre distintas interfaces de inicio de sesion

--- src/javafxmlapplication/FXMLVistaInicialController.java
+++ src/javafxmlapplication/FXMLVistaInicialController.java
@@ -1,14 +1,23 @@
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.ScaleTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -35,6 +44,8 @@ public class FXMLVistaInicialController implements Initializable {
     private PieChart tartaCategorias;
     @FXML
     private ImageView imagenPerfil;
+    @FXML
+    private VBox menu;
 
     /**
      *
@@ -43,6 +54,27 @@ public class FXMLVistaInicialController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+       // Crear una transición de escala
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(300), menu);
+        scaleTransition.setToX(1.5);
+        scaleTransition.setToY(1.0);
+
+        
     }    
+
+    @FXML
+    private void inicioSesion(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("FXMLVistaInicioSesion.fxml"));
+        Parent root = loader.load();
+        FXMLVistaInicioSesionController controller = loader.getController();
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setTitle("Inicio Sesion");
+        stage.setResizable(false);
+        stage.showAndWait();
+    }
     
 }
--- src/javafxmlapplication/FXMLVistaInicioSesionController.java
+++ src/javafxmlapplication/FXMLVistaInicioSesionController.java
@@ -4,9 +4,18 @@
  */
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +24,9 @@ import javafx.fxml.Initializable;
  */
 public class FXMLVistaInicioSesionController implements Initializable {
 
+    @FXML
+    private Button registroButton;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +34,20 @@ public class FXMLVistaInicioSesionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void registrarse(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("FXMLVistaRegistro.fxml"));
+        Parent root = loader.load();
+        FXMLVistaRegistroController controller = loader.getController();
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setTitle("Registrarse");
+        stage.setResizable(false);
+        stage.showAndWait();
+    }
     
 }
