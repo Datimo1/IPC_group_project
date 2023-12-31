3a012ae002f2c7947aa3591cd39e82d8e930a19c
ngalvezllor <90264555+ngalvezllor@users.noreply.github.com>
2023-12-31 12:24:53 +0100

    vista gastos

--- src/javafxmlapplication/FXMLVistaInicialController.java
+++ src/javafxmlapplication/FXMLVistaInicialController.java
@@ -4,8 +4,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 /**
@@ -29,6 +31,10 @@ public class FXMLVistaInicialController implements Initializable {
     private Button graficasButton;
     @FXML
     private Button aPDFButton;
+    @FXML
+    private PieChart tartaCategorias;
+    @FXML
+    private ImageView imagenPerfil;
 
     /**
      *
